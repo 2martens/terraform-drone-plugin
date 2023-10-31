33cd078958803ddea98a1440169efacb2fb3f477
Jim Martens <github@2martens.de>
2023-10-31 15:51:02 +0100

    Restructured tool.sh

--- tool.sh
+++ tool.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cd "$PLUGIN_PATH" && /usr/local/bin/tfci --organization="$PLUGIN_CLOUD_ORGANIZATION" --token="$PLUGIN_API_TOKEN" $PLUGIN_ARGS
+cd "$PLUGIN_PATH" && \
+ /usr/local/bin/tfci \
+  -organization="$PLUGIN_CLOUD_ORGANIZATION" \
+  -token="$PLUGIN_API_TOKEN" $PLUGIN_ARGS
