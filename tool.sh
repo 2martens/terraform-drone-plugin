7affa009ca46551eaca7dd630a835244417a81ad
Jim Martens <github@2martens.de>
2023-10-31 15:35:13 +0100

    Added command to execute in a specified path from the root of the git repo

--- tool.sh
+++ tool.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/bin/tfci --organization="$PLUGIN_CLOUD_ORGANIZATION" --token="$PLUGIN_API_TOKEN" $PLUGIN_ARGS
+cd "$PLUGIN_PATH" && /usr/local/bin/tfci --organization="$PLUGIN_CLOUD_ORGANIZATION" --token="$PLUGIN_API_TOKEN" $PLUGIN_ARGS
