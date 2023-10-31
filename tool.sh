834825ff5389e94dd24d9fd202a017ba273ad82e
Jim Martens <github@2martens.de>
2023-10-31 23:20:28 +0100

    Update tool to store output in JSON format in working directory

--- tool.sh
+++ tool.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-cd "$PLUGIN_PATH" && \
- TF_TOKEN_app_terraform_io=$PLUGIN_API_TOKEN \
- terraform $PLUGIN_ARGS
+working_dir=$(pwd)
+cd "$PLUGIN_PATH" || exit 1
+terraform init
+if [ "$PLUGIN_AUTO_APPROVE" = "true" ]
+then
+  auto_approve=" -auto-approve"
+else
+  auto_approve=""
+fi
+TF_TOKEN_app_terraform_io=$PLUGIN_API_TOKEN terraform apply -input=false"$auto_approve"
+TF_TOKEN_app_terraform_io=$PLUGIN_API_TOKEN terraform output -json > "$working_dir/terraform_output"
+cd "$working_dir" || exit 1
